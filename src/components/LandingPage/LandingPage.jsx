45ace34c46a4a3464ef60516f24981d9c6e03c75
sub0-l3 <subhrajit.debnath@gmail.com>
2020-03-26 13:59:22 +0530

    Use prettier for code formatting

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,26 +1,24 @@
-import React, { Component } from 'react';
-import VideoPlayer from '../VideoPlayer'
-import './LandingPage.scss'
+import React, { Component } from "react";
+import VideoPlayer from "../VideoPlayer";
+import "./LandingPage.scss";
 
 class LandingPage extends Component {
   static defaultProps = {
     shouldDisplayMenu: true
-  }
-
+  };
 
   render() {
-      const url = "https://firebasestorage.googleapis.com/v0/b/net-ninja-marioplan-5bc26.appspot.com/o/maldives-speedboat.mp4?alt=media&token=dd4611f3-ac59-4bb2-bd2e-13ff6e5468ca"
-      const baseClassName = 'psa-landing-page';
-    return(
-        <div className={`${baseClassName}`}>
-            <VideoPlayer url={url}/>
-        </div>
+    const url =
+      "https://firebasestorage.googleapis.com/v0/b/net-ninja-marioplan-5bc26.appspot.com/o/maldives-speedboat.mp4?alt=media&token=dd4611f3-ac59-4bb2-bd2e-13ff6e5468ca";
+    const baseClassName = "psa-landing-page";
+    return (
+      <div className={`${baseClassName}`}>
+        <VideoPlayer url={url} />
+      </div>
     );
   }
 }
 
-LandingPage.propTypes = {
-
-};
+LandingPage.propTypes = {};
 
 export default LandingPage;
--- src/components/VideoPlayer/VideoPlayer.jsx
+++ src/components/VideoPlayer/VideoPlayer.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import DownloadLink from "react-download-link";
 import {
   FacebookShareButton,
@@ -12,66 +12,63 @@ import {
   TwitterIcon,
   TelegramIcon,
   WhatsappIcon,
-  EmailIcon,
-} from 'react-share';
+  EmailIcon
+} from "react-share";
 
-import './VideoPlayer.scss'
+import "./VideoPlayer.scss";
 class VideoPlayer extends Component {
   static defaultProps = {
     shouldDisplayMenu: true
-  }
-
+  };
 
   render() {
-      const {url} = this.props
-      const baseClassName = 'psa-video-player';
-    return(
-        <div className={`${baseClassName}`}>
-            <video width="320" height="240" controls>
-                <source src={url} type="video/mp4" />
-                {/* <source src="movie.ogg" type="video/ogg" /> */}
-                Your browser does not support the video tag.
-            </video>
+    const { url } = this.props;
+    const baseClassName = "psa-video-player";
+    return (
+      <div className={`${baseClassName}`}>
+        <video width="320" height="240" controls>
+          <source src={url} type="video/mp4" />
+          {/* <source src="movie.ogg" type="video/ogg" /> */}
+          Your browser does not support the video tag.
+        </video>
 
-            <div className={`${baseClassName}__download-div`}>
-              <DownloadLink
-                  label="Download Video"
-                  filename="myfile.mp4"
-                  exportFile={() => url}
-              />
-            </div>
-            <div className={`${baseClassName}__share-div`}>
-            <FacebookShareButton url={url}>
-              <FacebookIcon size="50" round={true}/>
-            </FacebookShareButton>
-
-            <LinkedinShareButton url={url}>
-              <LinkedinIcon size="50" round={true} />
-            </LinkedinShareButton>
-
-            <TwitterShareButton url={url}>
-              <TwitterIcon size="50" round={true} />
-            </TwitterShareButton>
-
-            <TelegramShareButton url={url}>
-              <TelegramIcon size="50" round={true} />
-            </TelegramShareButton>
-
-            <WhatsappShareButton url={url}>
-              <WhatsappIcon size="50" round={true} />
-            </WhatsappShareButton>
-
-            <EmailShareButton url={url}>
-              <EmailIcon size="50" round={true} />
-            </EmailShareButton>
-            </div>
+        <div className={`${baseClassName}__download-div`}>
+          <DownloadLink
+            label="Download Video"
+            filename="myfile.mp4"
+            exportFile={() => url}
+          />
         </div>
+        <div className={`${baseClassName}__share-div`}>
+          <FacebookShareButton url={url}>
+            <FacebookIcon size="50" round={true} />
+          </FacebookShareButton>
+
+          <LinkedinShareButton url={url}>
+            <LinkedinIcon size="50" round={true} />
+          </LinkedinShareButton>
+
+          <TwitterShareButton url={url}>
+            <TwitterIcon size="50" round={true} />
+          </TwitterShareButton>
+
+          <TelegramShareButton url={url}>
+            <TelegramIcon size="50" round={true} />
+          </TelegramShareButton>
+
+          <WhatsappShareButton url={url}>
+            <WhatsappIcon size="50" round={true} />
+          </WhatsappShareButton>
+
+          <EmailShareButton url={url}>
+            <EmailIcon size="50" round={true} />
+          </EmailShareButton>
+        </div>
+      </div>
     );
   }
 }
 
-VideoPlayer.propTypes = {
-
-};
+VideoPlayer.propTypes = {};
 
 export default VideoPlayer;
