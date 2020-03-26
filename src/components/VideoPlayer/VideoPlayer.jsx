c167016b4d56fab1794743daf1ae6a85bf568e3e
amit_edbot <amitk@mirafra.com>
2020-03-26 12:07:41 +0530

    master: share and download option added

--- src/components/VideoPlayer/VideoPlayer.jsx
+++ src/components/VideoPlayer/VideoPlayer.jsx
@@ -1,4 +1,20 @@
 import React, { Component } from 'react';
+import DownloadLink from "react-download-link";
+import {
+  FacebookShareButton,
+  LinkedinShareButton,
+  TwitterShareButton,
+  TelegramShareButton,
+  WhatsappShareButton,
+  EmailShareButton,
+  FacebookIcon,
+  LinkedinIcon,
+  TwitterIcon,
+  TelegramIcon,
+  WhatsappIcon,
+  EmailIcon,
+} from 'react-share';
+
 import './VideoPlayer.scss'
 class VideoPlayer extends Component {
   static defaultProps = {
@@ -11,11 +27,43 @@ class VideoPlayer extends Component {
       const baseClassName = 'psa-video-player';
     return(
         <div className={`${baseClassName}`}>
-            <video width="320" height="240" controls>
-                <source src={url} type="video/mp4" />
+            {/* <video width="320" height="240" controls>
+                <source src={url} type="video/mp4" /> */}
                 {/* <source src="movie.ogg" type="video/ogg" /> */}
-                Your browser does not support the video tag.
-            </video>
+                {/* Your browser does not support the video tag. */}
+            {/* </video> */}
+
+            
+            <DownloadLink
+                label="Download Video"
+                filename="myfile.mp4"
+                exportFile={() => url}
+            />
+            <div className={`${baseClassName}__share-div`}>
+            <FacebookShareButton url={url}>
+              <FacebookIcon size="50" round={true}/>
+            </FacebookShareButton>
+
+            <LinkedinShareButton url={url}>
+              <LinkedinIcon size="50" round={true} />
+            </LinkedinShareButton>
+
+            <TwitterShareButton url={url}>
+              <TwitterIcon size="50" round={true} />
+            </TwitterShareButton>
+
+            <TelegramShareButton url={url}>
+              <TelegramIcon size="50" round={true} />
+            </TelegramShareButton>
+
+            <WhatsappShareButton url={url}>
+              <WhatsappIcon size="50" round={true} />
+            </WhatsappShareButton>
+
+            <EmailShareButton url={url}>
+              <EmailIcon size="50" round={true} />
+            </EmailShareButton>
+            </div>
         </div>
     );
   }
