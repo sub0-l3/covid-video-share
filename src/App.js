d421ca2f15a4e47e6f5082329cf8ce7d347215d5
sub0-l3 <subhrajit.debnath@gmail.com>
2020-03-27 01:40:28 +0530

    Connect app to firebase

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
-import React from 'react';
-import LandingPage from './components/LandingPage';
-import './App.css';
+import React, { Component } from "react";
+import LandingPage from "./components/LandingPage";
+import { auth } from "./services/firebase";
+import { signin } from "./helpers/auth";
+import { readUserData } from "./helpers/db";
 
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      authenticated: false
+    };
+  }
+  async componentDidMount() {
+    await signin("subhrajit.debnath@gmail.com", "subzero");
+
+    auth().onAuthStateChanged(user => {
+      if (user) {
+        readUserData(user.uid)
+        this.setState({
+          authenticated: true
+        });
+      } else {
+        this.setState({
+          authenticated: false
+        });
+      }
+    });
+  }
+  render() {
+    return (
+      <div className="App">
         <LandingPage />
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
