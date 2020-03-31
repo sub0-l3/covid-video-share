63d8af0c2d4237237c519c5634c7381e5373633b
amit_edbot <amitk@mirafra.com>
2020-03-31 14:58:34 +0530

    master: adding back button

--- src/components/NavigationBar/NavigationBar.jsx
+++ src/components/NavigationBar/NavigationBar.jsx
@@ -14,7 +14,9 @@ class NavigationBar extends Component {
     };
   }
 
-
+goBackBtn = () => {
+  window.history.back();
+}
   render() {
     const baseClassName = "psa-navigation-bar";
 
@@ -22,7 +24,7 @@ class NavigationBar extends Component {
     return (
       <div className={`${baseClassName}`}>
         <ul className={`${baseClassName}__nav-ul`}>
-            <li><a className="active" href={process.env.REACT_APP_LINK_HOME}>Back</a></li>
+            <li><a href="# " className="active" onClick={()=>this.goBackBtn()}>Back</a></li>
             <li><a href={process.env.REACT_APP_LINK_VIDEO_LIB}>Video library</a></li>
             <li><a href={process.env.REACT_APP_LINK_REDIRECT_UNAUTHORIZED}>Logout</a></li>
         </ul>
