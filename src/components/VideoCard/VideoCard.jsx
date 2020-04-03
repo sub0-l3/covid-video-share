6f6252ba4085f64f9a34b60d883363b6cecdb463
amit_edbot <amitk@mirafra.com>
2020-04-03 13:31:59 +0530

    master: download issue fixed

--- src/components/VideoCard/VideoCard.jsx
+++ src/components/VideoCard/VideoCard.jsx
@@ -1,8 +1,5 @@
 import React, { Component } from "react";
-// import { Link } from "react-router-dom";
-// import axios from 'axios'
-// import fileDownload from 'js-file-download';
-// import DownloadLink from "react-download-link";
+import axios from 'axios'
 import { withRouter } from "react-router-dom";
 import firebase from "firebase";
 import Moment from 'react-moment'
@@ -32,53 +29,26 @@ class VideoCard extends Component {
     if (this.props.redirect)
       this.props.history.replace(`/videos/${this.props.psaId}`);
   };
+
  downloadFile = () => {
-  // firebase
-  // .storage()
-  // .ref("videos")
-  // .child(this.props.videoId)
-  // .getDownloadURL()
-  // .then(url => console.log(url));
-
-  firebase
-  .storage().ref("videos").child(this.props.videoId).getDownloadURL().then(function(url) {
-    // `url` is the download URL for 'images/stars.jpg'
-  
-    // This can be downloaded directly:
-    var xhr = new XMLHttpRequest();
-    xhr.responseType = 'blob';
-    xhr.onload = function(event) {
-      var blob = xhr.response;
-      console.log(blob)
-    };
-    xhr.open('GET', url);
-    xhr.send();
-    // Or inserted into an <img> element:
-    // var img = document.getElementById('myimg');
-    // img.src = url;
-  }).catch(function(error) {
-    // Handle any errors
-    console.log(error)
+  var config = {
+    headers: {'Access-Control-Allow-Origin': '*'}
+  };
+    axios({
+      url: this.props.url,
+      method: 'GET',
+      responseType: 'blob', // important
+      config
+    }).then((response) => {
+      const url = window.URL.createObjectURL(new Blob([response.data]));
+      const link = document.createElement('a');
+      link.href = url;
+      link.setAttribute('download', this.props.outputVideoId);
+      document.body.appendChild(link);
+      link.click();
   });
-
-  // console.log(this.state.avatarURL);
-//   var config = {
-//     headers: {'Access-Control-Allow-Origin': '*'}
-// };
-//   axios({
-//     url: this.props.url,
-//     method: 'GET',
-//     responseType: 'blob', // important
-//     config
-//   }).then((response) => {
-//     const url = window.URL.createObjectURL(new Blob([response.data]));
-//     const link = document.createElement('a');
-//     link.href = url;
-//     link.setAttribute('download', 'file.mp4');
-//     document.body.appendChild(link);
-//     link.click();
-//   });
  }
+
   render() {
     const { url, name, date } = this.props;
     const baseClassName = "psa-video-card";
@@ -132,7 +102,7 @@ class VideoCard extends Component {
           />
 
       <a href={url} download="a.mp4">download me</a> */}
-      <button onClick={()=>this.downloadFile()} className={`${baseClassName}__download-button`}><FaDownload color={"white"} /></button>
+      <button onClick={()=>this.downloadFile()} className={`${baseClassName}__download-button`}><FaDownload color={"white"}/></button>
         </div>
       </div>
     );
